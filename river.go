1afb07d88882c394f352612af752c5b4487019e9
Will Alexander <github.com@willalexander.me>
2015-06-23 15:17:57 -0400

    Add file type and fix command line args

--- river.go
+++ river.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 // #cgo LDFLAGS: -lavformat -lavutil
@@ -12,29 +12,46 @@ import (
 // #include "libavformat/avformat.h"
 import "C"
 
-func tags(filename string) (tags map[string]string, err error) {
-	var fmtCtx *C.AVFormatContext = nil
-	if ret := C.avformat_open_input(&fmtCtx, C.CString("sintel.flac"), nil, nil); ret != 0 {
-		return nil, errors.New("C.avformat_open_input")
+type tag struct {
+	name string
+	value string
+}
+
+type file struct {
+	fmtCtx *C.AVFormatContext
+}
+
+func open(name string) (f file, err error) {
+	var fmtCtx *C.AVFormatContext
+	if ret := C.avformat_open_input(&fmtCtx, C.CString(name), nil, nil); ret != 0 {
+		return file{}, errors.New("C.avformat_open_input")
 	}
+	return file{fmtCtx: fmtCtx}, nil
+}
+
+func (f file) close() {
+	C.avformat_close_input(&f.fmtCtx)
+}
+
+func (f file) readTags() (tags map[string]string) {
 	emptyCString := C.CString("")
 	tags = make(map[string]string)
-	for i := C.av_dict_get(fmtCtx.metadata, emptyCString, nil, C.AV_DICT_IGNORE_SUFFIX);
+	for i := C.av_dict_get(f.fmtCtx.metadata, emptyCString, nil, C.AV_DICT_IGNORE_SUFFIX);
 		i != nil;
-		i = C.av_dict_get(fmtCtx.metadata, emptyCString, i, C.AV_DICT_IGNORE_SUFFIX) {
+		i = C.av_dict_get(f.fmtCtx.metadata, emptyCString, i, C.AV_DICT_IGNORE_SUFFIX) {
 		tags[C.GoString(i.key)] = C.GoString(i.value)
 	}
-	C.avformat_close_input(&fmtCtx)
 	return
 }
 
-var filename = flag.String("filename", "", "the file to read tags from")
-
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("no filename provided")
+	}
 	C.av_register_all()
-	tags, err := tags(*filename)
+	file, err := open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(tags)
+	fmt.Println(file.readTags())
 }
